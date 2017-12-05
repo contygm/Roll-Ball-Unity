d0d16fd71d83be50a71cd7d861a6fbfdc374c515
Genevieve Conty <genevieve.conty@AV-MBP-7208.hitronhub.home>
2017-12-05 08:16:28 -0800

    the ball now rolls around the scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// public variables show up as editable properties in Unity
+	public float speed;
+
+	// don't show up in the Unity as editable properties
+	private Rigidbody rb;
+
+	// runs once at the start of the game
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame/when rendering a frame
+	// most game code goes here
+	void Update () 
+	{
+		
+	}
+
+	// FixedUpdate is called just before doing physics calculations
+	// physics code goes here
+	void FixedUpdate () 
+	{
+		
+		float moveHorizationtal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		// Vector3 (x ,y ,z)
+		Vector3 movement = new Vector3 (moveHorizationtal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
