d82466125ba30da7ba099a431fa02df8ba99d563
Genevieve Conty <genevieve.conty@AV-MBP-7208.local>
2017-12-07 12:55:31 -0800

    the game has walls now

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame
+	// waits for update to be called before running
+	// use this to make sure the player's move is done b4 running the code below
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
